1e2ac13e4819f6552011bed012f144fea9eaae6f
Oro31 <60878633+Oro31@users.noreply.github.com>
2021-12-17 22:47:21 +0100

    ne compile pas mais l'algo est la

---
+++ parse_dollar/ft_list_cmd.c
@@ -0,0 +1,78 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_list_cmd.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rvalton <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/06 14:05:31 by rvalton           #+#    #+#             */
+/*   Updated: 2021/12/13 01:21:11 by rvalton          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+t_cmd	*ft_hadd_tolist_cmd(t_cmd *list, char **split, int is_pipe, int pos)
+{
+	t_cmd	*newelem;
+
+	newelem = malloc(sizeof(t_cmd));
+	if (!newelem)
+	{
+		ft_print_str("malloc failed\n");
+		return (NULL);
+	}
+	newelem->argv = split;
+	newelem->is_cmd_after_pipe = is_pipe;
+	newelem->pos = pos;
+	newelem->nxt = list;
+	return (newelem);
+}
+
+t_cmd	*ft_eadd_tolist_cmd(t_cmd *list, char **split, int is_pipe, int pos)
+{
+	t_cmd	*newelem;
+	t_cmd	*tmp;
+
+	newelem = malloc(sizeof(t_cmd));
+	if (!newelem)
+	{
+		ft_print_str("malloc failed\n");
+		return (NULL);
+	}
+	newelem->argv = split;
+	newelem->is_cmd_after_pipe = is_pipe;
+	newelem->pos = pos;
+	newelem->nxt = NULL;
+	if (list == NULL)
+		return (newelem);
+	else
+	{
+		tmp = list;
+		while (tmp->nxt != NULL)
+			tmp = tmp->nxt;
+		tmp->nxt = newelem;
+		return (list);
+	}
+}
+
+void	ft_free_list_cmd(t_cmd **list, int del)
+{
+	t_cmd	*tmp;
+	int		i;
+
+	while (*list)
+	{
+		i = -1;
+		if ((*list)->argv)
+			while ((*list)->argv[++i])
+				free((*list)->argv[i]);
+		free((*list)->argv);
+		tmp = (*list)->nxt;
+		if (!tmp && del)
+			free(*list);
+		else if (tmp)
+			free(*list);
+		*list = tmp;
+	}
+}
